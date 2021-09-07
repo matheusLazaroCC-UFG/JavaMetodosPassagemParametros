dea601be38bb92602d07b96862c4751c0b3b1314
matheusLazaroCC-UFG <71093772+matheusLazaroCC-UFG@users.noreply.github.com>
2021-09-07 20:25:46 -0300

    Update SwapPassagemValorResultado.java

--- Exercicio1/SwapPassagemValorResultado.java
+++ Exercicio1/SwapPassagemValorResultado.java
@@ -1,13 +1,19 @@
+package Exercicio1;
+
 import java.util.Arrays;
 
 /**
  * A passagem por valor-resultado é um modelo de implementação para parâmetros
  * de entrada e saída no qual são copiados os valores reais.
+ *
+ * Java não permite este tipo de implementação.
  */
 public class SwapPassagemValorResultado {
-    private int i;
-    public void swap(int a, int b){
-        i = b;
+    public void swapPassagemValorResultado(int a, int b){
+        int temp = a;
+
+        a = b;
+        b = temp;
     }
 
     public static void main(String[] args){
@@ -19,21 +25,6 @@ public class SwapPassagemValorResultado {
         System.out.println("Valores originais:\n"+ "value = " + value + "\nlist = " + Arrays.toString(list));
 
 
-        System.out.println("Passagem 1 (swap de value = " + value + " com list[0] = " + list[0] + ")");
-        teste1.i = value;
-        list[teste1.i] = 5;
-        teste1.swap(value, list[0]);
-        System.out.println("value = " + value + "\nlist = " + Arrays.toString(list));
-/*
-        System.out.println("Passagem 2:(swap de list[0] = " + list[0] + " com list[1] = " + list[1] +")");
-        teste1.SwapPassagemValorResultado(list[0], list[1]);
-        System.out.println("value = " + value + "\nlist = " + Arrays.toString(list));
-
-        System.out.println("Passagem 3:(swap de value = " + value + " com list[value] = list["+ value +"] = " + list[value] + ")");
-        teste1.SwapPassagemValorResultado(value, list[value]);
-        System.out.println("value = " + value + "\nlist = " + Arrays.toString(list));
-        System.out.println("-------------------------------------------------------------");
-*/
 
     }
 }
